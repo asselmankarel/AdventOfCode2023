2bec03fa937b6d3f9054f964a7b44a2436e1b31b
KarelAsselman <karel.asselman@allphi.eu>
2023-11-30 18:56:41 +0100

    Add project files.

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Utils;
+
+var lines = FileReader.ReadFile("input.txt");
+
+foreach (var line in lines)
+{
+    if (line == Environment.NewLine)
+    {
+
+    } else
+    {
+
+    }
+    
+}
---
+++ Utils/FileReader.cs
@@ -0,0 +1,14 @@
+﻿namespace Utils;
+
+public static class FileReader
+{
+    public static string[] ReadFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        throw new FileNotFoundException();
+    }
+}
