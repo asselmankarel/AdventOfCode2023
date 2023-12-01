1fdc208ccb69dbb4ec2a6b9b63aaf05865bd692b
Karel Asselman <karel.asselman@allphi.eu>
2023-12-01 17:16:48 +0100

    Completed day 1 part 1

---
+++ Day1-Part1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Text.RegularExpressions;
+using Utils;
+
+var lines = FileReader.ReadFile("input.txt");
+int sum = 0;
+
+foreach (var line in lines)
+{
+    var result = Regex.Matches(line, @"[0-9]");
+    int number = int.Parse(result[0].ToString() + result[^1].ToString());
+    sum += number;
+}
+
+Console.WriteLine($"Answer: {sum}");
