bdb3fff7245a74e34bd53957d447c74cbe894789
KarelAsselman <karel.asselman@allphi.eu>
2023-12-01 16:44:01 +0100

    2022

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,16 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Utils;
+﻿using Utils;
 
 var lines = FileReader.ReadFile("input.txt");
 
+int max = 0;
+int sum = 0;
+List<int> totalByElf = new();
+
 foreach (var line in lines)
 {
-    if (line == Environment.NewLine)
+    if (line == string.Empty)
     {
+        if (sum > max)
+            max = sum;
 
-    } else
-    {
+        totalByElf.Add(sum);
+        sum = 0;
+    } 
 
-    }
-    
+    if (int.TryParse(line, out int value))
+        sum += value; 
 }
+
+int topThree = totalByElf.OrderByDescending(x => x).Take(3).Sum();
+
+Console.WriteLine($"The answer is: {max}");
+Console.WriteLine($"Top 3 total is: {topThree}");
