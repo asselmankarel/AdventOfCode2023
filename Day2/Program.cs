095e85980c7cf4702ff1a6b538ec429e248da8ef
Karel Asselman <karel.asselman@allphi.eu>
2023-12-02 13:09:36 +0100

    completed day2 part 1

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,6 +1,59 @@
-﻿using Utils;
+﻿using Day2;
+using Utils;
 
 var lines = FileReader.ReadFile("input.txt");
+var games = new List<Game>();
+int sum = 0;
 
-Console.WriteLine(lines);
+foreach(var line in lines) 
+  games.Add(ParseLineToGame(line));
+
+var validGames = games.Where(g => g.IsValid());
+sum = validGames.Sum(g => g.Id);
+
+Console.WriteLine($"Answer part 1: {sum}");
+
+
+Game ParseLineToGame(string line) 
+{
+  var game = new Game();
+  int startIndex = line.IndexOf(' ') + 1;
+  int endIndex = line.IndexOf(':');
+  game.Id = int.Parse(line[startIndex..endIndex]);
+
+  string[] setsStr = line.Substring(endIndex + 1, line.Length - endIndex -1).Split(';');
+  ParseSets(setsStr, game);
+
+  return game;
+}
+
+void ParseSets(string[] setsStr, Game game) 
+{
+   foreach(string setStr in setsStr)
+  {
+    var matches = setStr.Split(';');
+    foreach(string match in matches) 
+    {
+      var set = new Set();
+      var entries = match.Split(',');
+
+      foreach(string entry in entries)
+      {
+        var result = entry.Trim().Split(' ');
+        string cubes = result[0];
+        string color = result[1];
+
+        switch (color)
+        {
+          case "blue" : set.Blue  = int.Parse(cubes); break;
+          case "green": set.Green = int.Parse(cubes); break;
+          case "red"  : set.Red   = int.Parse(cubes); break;
+          default: break;
+        }
+      }
+        game.Sets.Add(set);
+    }
+  }
+}
+
 
---
+++ Day2/Set.cs
@@ -0,0 +1,8 @@
+namespace Day2;
+
+public class Set 
+{
+  public int Blue { get; set; } = 0;
+  public int Green { get; set; } = 0;
+  public int Red { get; set; } = 0;
+}
