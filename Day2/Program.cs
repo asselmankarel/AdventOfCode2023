79d34ef259550fcd6728eece517e52d8c06fc3bb
Karel Asselman <karel.asselman@allphi.eu>
2023-12-02 13:24:56 +0100

    completed day 2 part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -4,14 +4,27 @@ using Utils;
 var lines = FileReader.ReadFile("input.txt");
 var games = new List<Game>();
 int sum = 0;
+int sumOfPowers = 0;
 
 foreach(var line in lines) 
   games.Add(ParseLineToGame(line));
 
-var validGames = games.Where(g => g.IsValid());
-sum = validGames.Sum(g => g.Id);
-
+sum = games.Where(g => g.IsValid()).Sum(g => g.Id);
 Console.WriteLine($"Answer part 1: {sum}");
+
+
+// Part 2
+foreach(var game in games) 
+{
+  int maxBlue = game.Sets.Select(s => s).Max( s => s.Blue);
+  int maxGreen = game.Sets.Select(s => s).Max( s => s.Green);
+  int maxRed = game.Sets.Select(s => s).Max( s => s.Red);
+
+  int power = maxBlue * maxGreen * maxRed;
+  sumOfPowers += power;
+}
+
+Console.WriteLine($"Answer part 2: {sumOfPowers}");
 
 
 Game ParseLineToGame(string line) 
@@ -20,7 +33,6 @@ Game ParseLineToGame(string line)
   int startIndex = line.IndexOf(' ') + 1;
   int endIndex = line.IndexOf(':');
   game.Id = int.Parse(line[startIndex..endIndex]);
-
   string[] setsStr = line.Substring(endIndex + 1, line.Length - endIndex -1).Split(';');
   ParseSets(setsStr, game);
 
@@ -29,7 +41,7 @@ Game ParseLineToGame(string line)
 
 void ParseSets(string[] setsStr, Game game) 
 {
-   foreach(string setStr in setsStr)
+  foreach(string setStr in setsStr)
   {
     var matches = setStr.Split(';');
     foreach(string match in matches) 
@@ -51,7 +63,8 @@ void ParseSets(string[] setsStr, Game game)
           default: break;
         }
       }
-        game.Sets.Add(set);
+
+      game.Sets.Add(set);
     }
   }
 }
